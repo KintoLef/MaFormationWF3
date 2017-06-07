667f7c9a49bd912a394c6061f217e6f5a409a8b9
Quentin LEFEVRE <quinto_lefevre@hotmail.com>
2017-06-07 16:59:37 +0200

    Travail 07/06
    Suite Angular

--- ANGULAR/MonAppAngular/src/app/app.component.ts
+++ ANGULAR/MonAppAngular/src/app/app.component.ts
@@ -1,12 +1,6 @@
 // -- 1. Importation de la Librairie Angular Core
 import { Component } from '@angular/core';
-
-// -- Déclaration d'une Classe Contact
-export class Contact {
-  id : number;
-  fullname: string;
-  username: string;
-}
+import { Contact } from './shared/models/contact'
 
 // -- 2. Déclaration du Composant
 @Component({
@@ -15,73 +9,10 @@ export class Contact {
   selector: 'app-root',
   
   // -- 2.b : Le contenu HTML de notre composant
-  // templateUrl: './app.component.html',
-  template: `
-    <header>
-      <nav class="navbar navbar-inverse">
-        <div class="navbar-header">
-          <a href="#" class="navbar-brand">Mes Contacts</a>
-        </div>
-      </nav>
-    </header>
-
-    <div class="row">
-      <div class="col-sm-4">
-
-        <!-- *ngIf: Permet de faire le re,du HTML de la div, que si le tableau Contacts contient des objets -->
-        <div *ngIf="Contacts">
-          
-          <ul class="list-group list-contacts">
-            <li class="list-group-item" *ngFor="let contact of Contacts" (click)="choisirContact(contact)" [class.active]="contact === contactActif">
-              {{contact.fullname}} <i>({{contact.username}})</i>
-            </li>
-          </ul>
-
-        </div> <!-- /div if Contacts -->
-
-      </div> <!-- /div .col-sm-4 -->
-
-      <div class="col-sm-8">
-
-        <div class="jumbotron" *ngIf="contactActif">
-          
-          <h2>{{contactActif.fullname}}</h2>
-          <small>{{contactActif.username}}</small>
-
-          <!-- Avec l'expression {{ }} j'affiche le contenu de la variable dans l'application -->
-          <!-- <h3 class="text-center">Gestion de mes {{title}} !</h3> -->
-
-        </div> <!-- /div .jumbotron -->
-
-        <div class="jumbotron" *ngIf="!contactActif">
-          
-          <span class="glyphicon glyphicon-hand-left"></span>
-          <h2>Bonjour, choisis un contact !</h2>
-
-        </div> <!-- /div .jumbotron -->
-
-      </div> <!-- /div .col-sm-8 -->
-
-    </div> <!-- /div .row -->
-
-    
-
-    <!-- <p>Bonjour {{Contact.fullname}} <i>({{Contact.username}})</i></p> -->
-
-    
-
-    <footer class="text-center">
-      Copyright &copy; 2017
-    </footer>
-  `,
+  templateUrl: './app.component.html',
   
   // -- 2.c : Les styles CSS
-  // styleUrls: ['./app.component.css']
-  styles: [`
-    .list-contacts li {
-      cursor: pointer;
-    }
-  `]
+  styleUrls: ['./app.component.css']
 })
 
 // -- Notre code JS
--- ANGULAR/MonAppAngular/src/app/app.module.ts
+++ ANGULAR/MonAppAngular/src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { ProfilContactComponent } from './profil-contact/profil-contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfilContactComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
