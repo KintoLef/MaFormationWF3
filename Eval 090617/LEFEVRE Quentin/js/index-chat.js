d8b6a43791d7b2b7d864970df151508ebd546064
Quentin LEFEVRE <quinto_lefevre@hotmail.com>
2017-06-09 09:48:02 +0200

    Eval 09/06

---
+++ Eval 090617/LEFEVRE Quentin/js/index-chat.js
@@ -0,0 +1,43 @@
+// -- Initialisation de jQuery
+$(function() {
+        
+    $("#adopt-form").on("submit", function(event) {
+        
+        event.preventDefault();
+
+        // -- Déclaration des variables
+        var select   = $("#choixchat");
+        var message  = $("#message");
+
+        // -- Je passe à la vérification de chaque champs
+
+            // -- 1. Vérification du champ Choix du Chat
+            if(select.val() === null) {
+                select.parent().addClass("has-error");
+
+                $("<p class='text-danger'>Vous devez choisir un chat</p>").appendTo(select.parent());
+            };
+            
+            // -- 2. Vérification du Message (textarea)
+            if(message.val().length < 15) {
+                message.parent().addClass("has-error");
+                $("<p class='text-danger'>Vous devez saisir un minimum de 15 caractères</p>").appendTo(message.parent());
+            };
+
+            
+        if($(this).find(".has-error"). length == 0) {
+            $(this).replaceWith("<div class='alert alert-success'>Votre demande d'adoption à été transmise. Après étude de celle-ci, vous recevrez une réponse sous 15 jours.</div>");
+        }        
+    });
+    
+    // -- Suppression des différentes erreurs
+    $("#adopt-form #choixchat").on("focus", function() {
+        $("#adopt-form .selectchat .has-error").removeClass("has-error");
+        $("#adopt-form .selectchat .text-danger").remove();
+    });
+
+    $("#adopt-form #message").on("keyup", function() {
+        $("#adopt-form .messagechat .has-error").removeClass("has-error");
+        $("#adopt-form .messagechat .text-danger").remove();
+    });
+});
